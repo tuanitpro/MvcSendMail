7fc74287e598b9d6e19231bb5d114f85dabcf342
Lê Thanh Tuấn <tuanitpro@users.noreply.github.com>
2018-04-23 17:43:01 +0700

    Add files via upload

---
+++ MvcSendMail/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MvcSendMail.MvcApplication" Language="C#" %>
---
+++ MvcSendMail/Models/ContactModel.cs
@@ -0,0 +1,40 @@
+﻿/* FileName: ContactModel.cs
+Project Name: MvcSendMail
+Date Created: 9/15/2014PM
+Description: Auto-generated
+Version: 1.0.0.0
+Author:	Lê Thanh Tuấn - Khoa CNTT
+Author Email: tuanitpro@gmail.com
+Author Mobile: 0976060432
+Author URI: http://tuanitpro.com
+License: 
+
+*/
+using System.ComponentModel.DataAnnotations;
+namespace MvcSendMail.Models
+{
+    /// <summary>
+    /// Contact Model
+    /// Chứa các thuộc tính của form liên hệ
+    /// </summary>
+    public class ContactModel
+    {
+        [Display(Name = "Tên của bạn")]
+        [Required(ErrorMessage = "Tên không được trống")]
+        public string UserName { get; set; }
+
+        [Display(Name = "Email")]
+        [Required(ErrorMessage = "Email không được trống")]
+        [DataType(DataType.EmailAddress, ErrorMessage = "Email không đúng")]
+        public string Email { get; set; }
+
+        [Display(Name = "Chủ đề")]
+        [Required(ErrorMessage = "Chủ đề không được trống")]
+        public string Subject { get; set; }
+
+        [Display(Name = "Nội dung")]
+        [DataType(DataType.MultilineText)]
+        public string Message { get; set; }
+
+    }
+}
